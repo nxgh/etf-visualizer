894ee3972e4a035eccdcd14404369a1b1c3f7ed9
nxgh <hgxn1101@gmail.com>
2025-04-24 17:26:55 +0800

    feat: 添加交易管理功能并优化代码结构
    - 新增交易管理功能，包括交易记录的增删改查
    - 引入 `decimal.js` 进行精确计算
    - 优化代码结构，将交易相关逻辑移至 `stores/transaction.ts`
    - 更新 `tsconfig.json` 以支持新的路径别名
    - 修复日期选择器样式问题

---
+++ apps/frontend/utils/createEnums.ts
@@ -0,0 +1,48 @@
+/**
+ * 根据传入的对象生成枚举工具对象。
+ * 每个 key 会生成一个包含 key、value、toString 方法的对象，便于类型安全和 IDE 智能提示。
+ *
+ * @template T - 枚举对象的类型，key 为枚举值，value 为显示文本
+ * @param {T} data - 枚举对象，例如 {apple: '苹果', banana: '🍌' }
+ * @returns {{ [K in keyof T]: { value: T[K]; key: K;  } }}
+ * @example
+ * const TabEnum = createEnums({apple: '苹果', banana: '🍌' });
+ *
+ * TabEnum.apple.value // '苹果'
+ * TabEnum.apple.key // 'apple'
+ * TabEnum.banana.key // 'banana'
+ * TabEnum.banana.value // '🍌'
+ */
+export function createEnums<T extends Record<string, string>>(data: T) {
+  type EnumItem<K extends keyof T> = {
+    readonly value: T[K];
+    readonly key: K;
+  };
+
+  const result = {} as { [K in keyof T]: EnumItem<K> };
+
+  for (const key of Object.keys(data) as Array<keyof T>) {
+    const enumItem = {
+      key: key,
+      value: data[key],
+    };
+    Object.freeze(enumItem); // 防止属性被修改
+    result[key] = enumItem;
+  }
+
+  return Object.freeze(result);
+}
+
+/**
+ * @example
+ * type kt = GetCreateEnumsKeyType<typeof TabEnum>; // "apple" | "banana"
+ */
+export type GetCreateEnumsKeyType<T> = keyof T;
+
+/**
+ * @example
+ * type vt = GetCreateEnumsValueType<typeof TabEnum>; // "苹果" | "🍌"
+ */
+export type GetCreateEnumsValueType<T> = T extends { [K in keyof T]: { value: infer V } } ? V : never;
+
+export default createEnums;
